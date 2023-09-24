4a7ef66eddf5fc5d18c965f971926f61e4d1955a
GyanendraMani <gyanendramanimishra@gmail.com>
2023-09-25 01:05:01 +0530

    added register and login route

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const socket = require("socket.io");
 const InitiateMongoServer = require("./config/database");
-
+const authRoutes =require("./routes/auth");
 require('dotenv').config();
 
 const PORT = process.env.PORT;
@@ -14,8 +14,33 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("now sever is running ")
-})
+app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => console.log(`server is running on http://localhost:${PORT}`))
+// app.get('/', (req, res) => {
+//     res.send("now sever is running ")
+// })
+
+const server = app.listen(PORT, () => console.log(`server is running on http://localhost:${PORT}`))
+
+
+const io = socket(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        credentials: true,
+    },
+});
+
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+    global.chatSocket = socket;
+    socket.on("add-user", (userId) => {
+        onlineUsers.set(userId, socket.id);
+    });
+
+    socket.on("send-msg", data => {
+        const sendUserSocket = onlineUsers.get(data.to);
+        if (sendUserSocket) {
+            socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+        }
+    })
+})
---
+++ server/routes/auth.js
@@ -0,0 +1,8 @@
+const { login, register } = require("../controllers/userController");
+const router = require("express").Router();
+
+router.post("/register", register);
+router.post("/login", login);
+
+
+module.exports = router;
